adce29c62b472fdbe5a3d96ef519c0545b11331d
Ozomozyan <125055262+Ozomozyan@users.noreply.github.com>
2023-05-02 11:18:02 +0200

    modify

--- reception.php
+++ reception.php
@@ -4,18 +4,24 @@
 <?php
 
 $ID = $_GET['id'];
-$TITRE= $_GET['titre'];
+$TITRE = $_GET['titre'];
 $GENRE = $_GET['genre'];
 $ANNEE = $_GET['annee'];
 
-echo $ID ." ".$TITRE." ".$GENRE." ".$ANNEE; 
+echo $ID . " " . $TITRE . " " . $GENRE . " " . $ANNEE;
 
 //1° - Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20172939_movies', 'id20172939_root', 'C>3Gmt-4_2h3Fp)/');
 $base->exec("SET CHARACTER SET utf8");
-$sql = 'INSERT INTO movies VALUES('.$ID.',"'.$TITRE.'","'.$GENRE.'",'.$ANNEE.')';
-//2° - Préparation de requette et execution
-$retour = $base->query($sql);
+
+//2° - Préparation de requette et execution using prepared statements
+$sql = 'INSERT INTO movies (id, titre, genre, annee) VALUES (:id, :titre, :genre, :annee)';
+$stmt = $base->prepare($sql);
+$stmt->bindParam(':id', $ID, PDO::PARAM_INT);
+$stmt->bindParam(':titre', $TITRE, PDO::PARAM_STR);
+$stmt->bindParam(':genre', $GENRE, PDO::PARAM_STR);
+$stmt->bindParam(':annee', $ANNEE, PDO::PARAM_INT);
+$stmt->execute();
 
 ?>
 </br><a href="index.html">Retour à la page principale</a>
