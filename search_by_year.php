ae5b15fac139d66c0a4935a892755bb7cdcec27a
Ozomozyan <125055262+Ozomozyan@users.noreply.github.com>
2023-05-02 11:09:14 +0200

    modify

--- search_by_year.php
+++ search_by_year.php
@@ -1,14 +1,28 @@
 <html>
 <head>
-    <title>Recherche de films par année</title>
+    <title>Recherche de films par année (liste déroulante)</title>
 </head>
 <body>
-<h1>Recherche de films par année</h1>
+<h1>Recherche de films par année (liste déroulante)</h1>
 
-<!-- Add a new form for searching movies by year -->
+<?php
+// 1° - Connexion à la BDD
+$base = new PDO('mysql:host=localhost; dbname=id20172939_movies', 'id20172939_root', 'C>3Gmt-4_2h3Fp)/');
+$base->exec("SET CHARACTER SET utf8");
+
+// Get the unique years from the database
+$yearsQuery = $base->query('SELECT DISTINCT annee FROM movies ORDER BY annee;');
+$years = $yearsQuery->fetchAll(PDO::FETCH_COLUMN, 0);
+?>
+
+<!-- Add a new form for searching movies by year using a dropdown list -->
 <form action="" method="post">
     <label for="annee">Rechercher un film par année :</label>
-    <input type="number" name="annee" id="annee" required>
+    <select name="annee" id="annee" required>
+        <?php foreach ($years as $year): ?>
+            <option value="<?= htmlspecialchars($year) ?>"><?= htmlspecialchars($year) ?></option>
+        <?php endforeach; ?>
+    </select>
     <input type="submit" value="Rechercher">
 </form>
 
@@ -16,10 +30,6 @@
 // If the form is submitted, process the data
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $searchYear = $_POST['annee'];
-
-    // 1° - Connexion à la BDD
-    $base = new PDO('mysql:host=localhost; dbname=id20172939_movies', 'id20172939_root', 'C>3Gmt-4_2h3Fp)/');
-    $base->exec("SET CHARACTER SET utf8");
 
     // 2° - Préparation de requette et execution
     $query = $base->prepare('SELECT * FROM movies WHERE annee = :annee;');
